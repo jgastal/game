884e3ad115a184bb502963f57506895ffd403a24
Jonas M. Gastal <jgastal@gmail.com>
2011-07-05 21:14:08 -0300

    Initial implementation of scrolling map.

--- map.cpp
+++ map.cpp
@@ -1,6 +1,60 @@
 #include "map.h"
 
+#include "rect.h"
+
+#define MIN_DIST 20
+
+using namespace libwidget;
+
 Map::Map()
 {
-	setColor(libwidget::green);
+	setImage("grass.png");
+	imgWidth = getImgWidth();
+	imgHeight = getImgHeight();
+
+	player = new Rect(this);
+	player->resize(10, 10);
+	player->move(MIN_DIST, MIN_DIST);
+	player->setColor(blue);
+
+	onKeyPressed(bind(this, &Map::movePlayer));
+}
+
+void Map::resize(int w, int h)
+{
+	libwidget::Widget::resize(w, h);
+	width = w;
+	height = h;
+}
+
+void Map::movePlayer(SDL_KeyboardEvent* ev)
+{
+	SDL_Rect playerGeo = player->getGeometry();
+	switch(ev->keysym.sym)
+	{
+		case SDLK_UP:
+			if(playerGeo.y > 0)
+				player->move(playerGeo.x, playerGeo.y -= 2);
+			if(offY > 0 && playerGeo.y <= MIN_DIST)
+				offY--;
+			break;
+		case SDLK_DOWN:
+			if(playerGeo.y + playerGeo.h < height)
+				player->move(playerGeo.x, playerGeo.y += 2);
+			if(offY + height < imgHeight && playerGeo.y + playerGeo.h >= height - MIN_DIST)
+				offY++;
+			break;
+		case SDLK_RIGHT:
+			if(playerGeo.x + playerGeo.w < width)
+				player->move(playerGeo.x += 2, playerGeo.y);
+			if(offX + width < imgWidth && playerGeo.x + playerGeo.w >= width - MIN_DIST)
+				offX++;
+			break;
+		case SDLK_LEFT:
+			if(playerGeo.x > 0)
+				player->move(playerGeo.x -= 2, playerGeo.y);
+			if(offX > 0 && playerGeo.x <= MIN_DIST)
+				offX--;
+			break;
+	}
 }
--- map.h
+++ map.h
@@ -1,12 +1,24 @@
 #ifndef MAP_H
 #define MAP_H
 
-#include <rect.h>
+#include "image.h"
 
-class Map : public libwidget::Rect
+using namespace libwidget;
+
+namespace libwidget {
+	class Rect;
+}
+
+class Map : public Image
 {
 	public:
 		Map();
+		virtual void resize(int w, int h);
+
+	private:
+		void movePlayer(SDL_KeyboardEvent *ev);
+		Rect *player;
+		int width, height, imgWidth, imgHeight;
 };
 
 #endif // MAP_H
